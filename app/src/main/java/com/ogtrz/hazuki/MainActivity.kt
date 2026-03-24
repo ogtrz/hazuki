661565f284e4ecda32e36d66c8d7e719a6445601
lokiedev <ogtrz@proton.me>
2026-03-24 19:21:07 +0800

    refactor: optimize imports

--- app/src/main/java/com/ogtrz/hazuki/MainActivity.kt
+++ app/src/main/java/com/ogtrz/hazuki/MainActivity.kt
@@ -4,29 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.RowScope
-import androidx.compose.foundation.layout.consumeWindowInsets
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Icon
-import androidx.compose.material3.NavigationBar
-import androidx.compose.material3.NavigationBarItem
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import cafe.adriel.voyager.navigator.Navigator
-import cafe.adriel.voyager.navigator.tab.CurrentTab
-import cafe.adriel.voyager.navigator.tab.LocalTabNavigator
-import cafe.adriel.voyager.navigator.tab.Tab
-import cafe.adriel.voyager.navigator.tab.TabNavigator
-import com.ogtrz.hazuki.features.browse.BrowseTab
-import com.ogtrz.hazuki.features.history.HistoryTab
 import com.ogtrz.hazuki.features.home.HomeScreen
-import com.ogtrz.hazuki.features.library.LibraryTab
-import com.ogtrz.hazuki.features.more.MoreTab
 import com.ogtrz.hazuki.ui.theme.HazukiTheme
 
 class MainActivity : ComponentActivity() {
--- app/src/main/java/com/ogtrz/hazuki/ui/theme/colorscheme/HazukiColorScheme.kt
+++ app/src/main/java/com/ogtrz/hazuki/ui/theme/colorscheme/HazukiColorScheme.kt
@@ -1,6 +1,5 @@
 package com.ogtrz.hazuki.ui.theme.colorscheme
 
-import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.ui.graphics.Color
